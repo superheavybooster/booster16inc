af9200a035fef463df176143984b7cabfda1d4f5
JamesKSPPatcher <AntonioMJames16@gmail.com>
2025-10-11 19:21:00 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function showHome() {
-  // Hide all sections
-  const sections = ['flight1', 'flight2', 'updates'];
-  sections.forEach(sec => document.getElementById(sec + '-section').classList.add('hidden'));
-
-  // Show welcome screen
-  document.getElementById('welcome-screen').classList.remove('hidden');
-}
